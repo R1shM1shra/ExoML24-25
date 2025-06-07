6a253f323144d2f319f1768b489cd7a053339351
R1shM1shra <rm.rishmishra@gmail.com>
2025-06-07 18:31:00 -0400

    Update randomtree.py

--- algorithims/randomtree.py
+++ algorithims/randomtree.py
@@ -1,17 +1,9 @@
-# Data Processing
 import pandas as pd
 import numpy as np
-
-# Modelling
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
 from scipy.stats import randint
-
-# Tree Visualisation
-from sklearn.tree import export_graphviz
-from IPython.display import Image
-import graphviz
 
 # Load data
 train_df = pd.read_csv(r"C:\Users\894054\PycharmProjects\PythonProject\tra1n_split_2.csv")
@@ -24,11 +16,14 @@ y_train = train_df.iloc[:, 1]
 X_test = test_df.iloc[:, 2:]
 y_test = test_df.iloc[:, 1]
 
+# define classifier and fit
 rf = RandomForestClassifier()
 rf.fit(X_train, y_train)
 
+# predict
 y_pred = rf.predict(X_test)
 
+# accuracy
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
 
@@ -41,7 +36,7 @@ print(cm)
 importances = rf.feature_importances_
 feature_names = X_train.columns
 
-# Combine into a DataFrame for cleaner display
+# display
 feature_importances = pd.DataFrame({
     'Feature': feature_names,
     'Importance': importances
